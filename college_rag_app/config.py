205548d5ecf60b63d3df0eaec8475ce8855829c5
muhammedashiqkm <ashiq.kottakuthm@gmail.com>
2025-08-08 00:24:24 +0530

    Update config.py

--- college_rag_app/config.py
+++ college_rag_app/config.py
@@ -29,7 +29,7 @@ class Config:
     ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "*").split(",")
     
      # In production, set this to your Memcached server's address.
-    RATELIMIT_STORAGE_URI = os.getenv("MEMCACHED_URL")
+    RATELIMIT_STORAGE_URI = os.getenv("MEMCACHED_URL","memcached://memcached:11211")
 
     # Database
     DATABASE_URL = os.getenv("DATABASE_URL")
